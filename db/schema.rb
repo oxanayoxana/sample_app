20ea0b232296d7237f439f85a8fe7dbe5edce203
oxanayoxana <oksana.oleshchenko@gmail.com>
2018-11-04 22:44:39 +0200

    Validate User name and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_062702) do
+ActiveRecord::Schema.define(version: 2018_11_04_163446) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
